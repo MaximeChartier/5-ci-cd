3d232e99ab488e33ea827920cc3064ca33a3e7dc
Maxime Chartier <maxime.chartier@orange.com>
2020-11-23 09:41:24 +0100

    Use REDIS URL

--- src/dbClient.js
+++ src/dbClient.js
@@ -1,17 +1,19 @@
 var redis = require("redis");
-const configure = require('./configure')
+const configure = require("./configure");
 
-const config = configure()
-var client = redis.createClient({
-  host: config.redis.host,
-  port: config.redis.port,
-  retry_strategy: () => {
-    return new Error("Retry time exhausted")
+const config = configure();
+var client = redis.createClient(
+  process.env.REDIS_URL || {
+    host: config.redis.host,
+    port: config.redis.port,
+    retry_strategy: () => {
+      return new Error("Retry time exhausted");
+    },
   }
-})
+);
 
-process.on('SIGINT', function() {
+process.on("SIGINT", function () {
   client.quit();
 });
 
-module.exports = client
+module.exports = client;
